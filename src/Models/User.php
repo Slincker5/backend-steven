49406fcc102c077e4d860fac71a8697b1953de9b
Slincker5 <hipersocial08@gmail.com>
2024-10-15 21:11:52 -0600

    refactorizar

--- src/Models/User.php
+++ src/Models/User.php
@@ -17,7 +17,7 @@ class User extends Database
 
     public function getInfo($user_uuid)
     {
-        $sql = 'SELECT user_uuid, username, rol, fecha, verificado FROM usuarios WHERE user_uuid = 1';
+        $sql = 'SELECT user_uuid, username, rol, fecha, verificado FROM usuarios WHERE user_uuid = ?';
         $consult = $this->ejecutarConsulta($sql, [$user_uuid]);
         $list = $consult->fetchAll(\PDO::FETCH_ASSOC);
         return $list;
