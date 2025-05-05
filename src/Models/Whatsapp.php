fcd992a0b6c1f2a997925b6c821cbe7297efc55b
Slincker5 <hipersocial08@gmail.com>
2025-05-05 00:09:56 -0600

    primer end point de green api

--- src/Models/Whatsapp.php
+++ src/Models/Whatsapp.php
@@ -160,15 +160,14 @@ public function obtenerInfoWhatsapp() {
 
     $data = json_decode($response, true);
 
-    #if (isset($data['wid'])) {
-    #    return [
-    #        'numero' => explode("@", $data['wid'])[0],
-    #        'nombre' => $data['name'] ?? 'Desconocido',
-    #        'foto'   => $data['avatar'] ?? null
-    #    ];
-    #}
+    if (isset($data['wid'])) {
+        return [
+            'phone' => explode("@", $data['wid'])[0],
+            'avatar'   => $data['avatar'] ?? 'https://cintillos-plazamundo.netlify.app/usuario.png'
+        ];
+    }
 
-    return $data;
+    return null;
 }
 
 
