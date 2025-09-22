695979ac27f859f489158e9185d3a128360f9ab7
Gerson Borja <hipersocial08@gmail.com>
2025-09-21 23:13:23 -0600

    fix bug: Creacion de Clase cliente

--- index.php
+++ index.php
@@ -105,7 +105,7 @@ $app->group('/category', function ($group) {
 
 $app->group('/messages', function ($group) {
     $group->post('/new', MensajeController::class . ':crearMensaje');
-    $group->get('/list', MensajeController::class . ':obtenerPlantilla');
+    $group->post('/list', MensajeController::class . ':obtenerPlantilla');
 })->add($validateJwtMiddleware);
 
 $app->group('/base', function ($group) {
